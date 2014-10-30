2cb363a1e5dd7e8e766b04aa7fc32d869d4b696e
kyokomi <kyoko1220adword@gmail.com>
2014-10-30 18:17:01 +0900

    fix: Androidでビルド通るように修正

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./proj.android/build_native.py -p 19 -b debug
+
--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -10,11 +10,22 @@ LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+ifeq ($(HOST_OS),windows)
+  CPP_FILES := $(shell dir $(LOCAL_PATH)/../../Classes/*.cpp /b/a-d/s)                   
+else
+  CPP_FILES := $(shell find $(LOCAL_PATH)/../../Classes -name *.cpp)                   
+endif
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_SRC_FILES := hellocpp/main.cpp
+LOCAL_SRC_FILES += $(CPP_FILES:$(LOCAL_PATH)/%=%)
+
+#LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+
+ifeq ($(HOST_OS),windows)
+  LOCAL_C_INCLUDES := $(shell dir $(LOCAL_PATH)/../../Classes /b/ad/s)                   
+else
+  LOCAL_C_INCLUDES := $(shell find $(LOCAL_PATH)/../../Classes -type d)                   
+endif
 
 LOCAL_STATIC_LIBRARIES := cocos2dx_static
 
